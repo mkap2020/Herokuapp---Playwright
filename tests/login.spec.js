1423e5b423b083f3760b23d071e990114350736c
mkap2020 <kapadiya.mudra@gmail.com>
2026-06-17 13:47:47 -0400

    Fix syntax error

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('SauceDemo login flow', () => {
-    
   test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill('standard_user');
@@ -21,29 +20,15 @@ test.describe('SauceDemo login flow', () => {
     await page.locator('[data-test="logout-sidebar-link"]').click();
     await expect(page).toHaveURL(/saucedemo/);
   });
-//await page.goto('https://kitchen.applitools.com/');
-await page.goto('https://www.saucedemo.com/');
-await page.locator('[data-test="username"]').click();
-await page.locator('[data-test="username"]').fill('standard_user');
-await page.locator('[data-test="password"]').click();
-await page.locator('[data-test="password"]').fill('secret_sauce');
-await page.locator('[data-test="login-button"]').click();
-await page.getByText('Swag Labs').click();
-await page.pause();
 
-
-
+  test('Login Test', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.getByText('Swag Labs').click();
+    await page.pause();
+  });
 });
-
-  test('Homepage Test', async ({ page }) => {
-    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
-    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-  });
-
-
-  test('Logout Test', async ({ page }) => {
-    await page.getByRole('button', { name: 'Open Menu' }).click();
-    await page.locator('[data-test="logout-sidebar-link"]').click();
-    await expect(page).toHaveURL(/saucedemo/);
-  });
